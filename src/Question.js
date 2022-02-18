29318febe6176504fe581e698c004e215dc2814e
Parvathy VD <paru@Parvathys-MacBook-Pro.local>
2022-02-18 10:45:48 -0800

    button onclick to one to dynamically show the icons

--- src/Question.js
+++ src/Question.js
@@ -1,18 +1,12 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-
-
 const Question = ({title,info}) => {
   const [showInfo, setShowInfo] = useState(true);
-  const toggleQuesionInfo = () => {
-      console.log('toggle this');
-      setShowInfo(!showInfo);
-  }
   return (
     <div>
-      <h4>{title} {showInfo ?  <AiOutlineMinus onClick={toggleQuesionInfo}/> : <AiOutlinePlus onClick={toggleQuesionInfo}/>} </h4>
-       <p>{showInfo ? info : ''} </p>
+      <h4>{title} <button onClick={()=>setShowInfo(!showInfo)}>{showInfo ? <AiOutlineMinus />: <AiOutlinePlus />}</button></h4>
+       <p>{showInfo && info} </p>
     </div>
   )
 };
